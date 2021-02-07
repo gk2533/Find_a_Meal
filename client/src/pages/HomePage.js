b7613b4d20f8f3b76fd4da6952ec7aac1491c104
gm1234-a1 <41346742+gm1234-a1@users.noreply.github.com>
2021-02-06 21:59:02 -0500

    Update HomePage.js

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -15,7 +15,42 @@ export default () => {
       }
       i++;
     }
+    import React from 'react';
 
+export default () => {
+  var soupkitchens = ["Zhihan", "Zamie", "Gabe", "Zhihan Chen"];
+
+  function searchMe(toFind)
+  {
+    var i = 0;
+    var newList = []
+    while(i<soupkitchens.length)
+    {
+      if(!soupkitchens[i].search(toFind))
+      {
+        newList[newList.length] = soupkitchens[i];
+      }
+      i++;
+    }
+    if(newList.length == soupkitchens.length)
+    {
+      return("Please try again: ");
+    }
+    return (
+      <ol>
+        {newList.map(variable => (
+          <h2 key={variable}>{variable}</h2>
+        ))}
+      </ol>
+    );
+  }
+
+  return (
+    <div>
+      <h1>{searchMe("")}</h1>
+    </div>
+  );
+};
     return (
       <ol>
         {newList.map(variable => (
