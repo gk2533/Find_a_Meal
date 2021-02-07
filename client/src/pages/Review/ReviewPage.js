92761598189541e650537dca7ef01f3802303e00
zhihanC <67808576+zhihanC@users.noreply.github.com>
2021-02-07 06:17:46 -0500

    Finished styling the ReviewPage

--- client/src/pages/Review/ReviewPage.js
+++ client/src/pages/Review/ReviewPage.js
@@ -1,6 +1,14 @@
 import React, { useState } from 'react';
 import 'semantic-ui-css/semantic.min.css';
-import { Container, Header, Form, Input, Button } from 'semantic-ui-react';
+import {
+  Container,
+  Header,
+  Form,
+  Input,
+  Select,
+  TextArea,
+  Button,
+} from 'semantic-ui-react';
 import './style.css';
 
 const style = {
@@ -17,7 +25,16 @@ const style = {
     marginTop: '1em',
     marginBottom: '1em',
   },
+  review: {},
 };
+
+const ratingOptions = [
+  { key: '5', text: '5', value: '5' },
+  { key: '4', text: '4', value: '4' },
+  { key: '3', text: '3', value: '3' },
+  { key: '2', text: '2', value: '2' },
+  { key: '1', text: '1', value: '1' },
+];
 
 export default () => {
   var soupkitchens = ['Zhihan', 'Zamie', 'Gabe', 'Zhihan Chen'];
@@ -59,26 +76,26 @@ export default () => {
 
   return (
     <div>
-      <div id="background-wrap">
-          <div class="x1">
-              <div class="cloud"></div>
-          </div>
+      <div id='background-wrap'>
+        <div class='x1'>
+          <div class='cloud'></div>
+        </div>
 
-          <div class="x2">
-              <div class="cloud"></div>
-          </div>
+        <div class='x2'>
+          <div class='cloud'></div>
+        </div>
 
-          <div class="x3">
-              <div class="cloud"></div>
-          </div>
+        <div class='x3'>
+          <div class='cloud'></div>
+        </div>
 
-          <div class="x4">
-              <div class="cloud"></div>
-          </div>
+        <div class='x4'>
+          <div class='cloud'></div>
+        </div>
 
-          <div class="x5">
-              <div class="cloud"></div>
-          </div>
+        <div class='x5'>
+          <div class='cloud'></div>
+        </div>
       </div>
       <Container>
         <Header as='h1' style={style.logo} textAlign='center'>
@@ -98,31 +115,33 @@ export default () => {
               placeholder='Search up soupkitchens alphabetically!'
             />
           </Form.Field>
+          {searchMe()}
+        </Form>
+
+        <Header as='h3'>Leave a review!</Header>
+        <Form>
+          <Form.Field
+            control={Select}
+            options={ratingOptions}
+            label='Rating'
+            placeholder='5'
+            search
+            searchInput={{ id: 'form-select-rating' }}
+          />
+          <Form.Field
+            id='form-textarea-control-opinion'
+            control={TextArea}
+            label='Review'
+            placeholder='Write your review here'
+          />
+          <Form.Field
+            id='form-button-control-public'
+            control={Button}
+            content='Submit'
+            label=''
+          />
         </Form>
       </Container>
-      {searchMe()}
-
-
-
-      <form class="ui form">
-        <div class="field">
-          <label>Leave a review!</label>
-          <textarea></textarea>
-        </div>
-
-        <div class="field">
-        <label>Please rate this soupkitchen from 1-5 (5 being best!)</label>
-        <select class="ui dropdown">
-          <option value="">Gender</option>
-          <option value="1">Male</option>
-          <option value="0">Female</option>
-        </select>
-        </div>
-
-        <div>
-          <button type="submit" class="ui button">Submit</button>
-        </div>
-      </form>
     </div>
   );
 };
