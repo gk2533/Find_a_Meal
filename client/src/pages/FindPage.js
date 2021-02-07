5bbc2aa001195afde371738f577e606055867aed
gm1234-a1 <41346742+gm1234-a1@users.noreply.github.com>
2021-02-06 23:16:19 -0500

    Update FindPage.js

--- client/src/pages/FindPage.js
+++ client/src/pages/FindPage.js
@@ -2,9 +2,88 @@ import React from 'react';
 
 export default () => {
 
+  const style =
+  {
+    h1: {
+      marginTop: '3em',
+      color:'red';
+    },
+
+
+    // h2: {
+    //   margin: '4em 0em 2em',
+    // },
+    // reviews: {
+    //   marginTop: '2em',
+    //   padding: '2em 0em',
+    // },
+    // yep: {
+    //   marginBottom: '300px',
+    // },
+    // 
+    // #fullContainer: {
+    // 	paddingTop: '10vh',
+    // 	paddingLeft: '5vw',
+    // 	paddingBottom: '10vh',
+    // 	fontSize: '20px',
+    // 	lineHeight: '2.5',
+    //   color: 'C9DBE9',
+    // 
+    // },
+
+    header:
+    {
+      color: 'C9DBE9',
+    }
+  }
+
   return (
     <div>
-      <p>Test</p>
+      <h1> hi </h1>
     </div>
-  );
+    // <div className='h-100'>
+    //   <HomeNavbar />
+    //     <div id="fullContainer">
+    //       <div id="block1" className="blocks">
+    //         <div id="block1left">
+    //           <img id="profilePicture" src={ProfileIcon}/>
+    //         </div>
+    //         <div id="block1right">
+    //           <h1 id="name">{initFirstName()} {initLastName()}</h1>
+    //           <h2 id="studentoralumni">{"Student"} in {initSchool()}</h2>
+    //           <h2>Class of <span id="gradYear">{initGradYear()}</span></h2>
+    //         </div>
+    //       </div>
+    //       <div id="block2" className="blocks">
+    //         <div id="block2left">
+    //           <h3 id="phoneNum"><img className="icons" src={PhoneIcon}/>Phone Number: {initPhoneNum()}</h3>
+    //           <h3 id="email"><img className="icons" src={EmailIcon}/>Email: {initEmail()}</h3>
+    //           <h3 id="hometown"><img className="icons" src={HomeIcon}/>Hometown: {initHometown()}</h3>
+    //         </div>
+    //         <div id="block2right">
+    //           <h2>Academics</h2>
+    //           <h3 id="school">School: {initSchool()}</h3>
+    //           <h3 id="major">Major: {initMajor()} {initMajorTwo()}</h3>
+    //           <h3 id="minor">Minor/Concentration: {initMinor()}</h3>
+    //           <h3 id="highSchool">High School: {initHighSchool()}</h3>
+    //         </div>
+    //       </div>
+    //       <div id="block3" className="blocks">
+    //         <div id="block3left">
+    //           <h2>Biography</h2>
+    //           <h3 id="biography">{initBio()}</h3>
+    //         </div>
+    //         <div id="block3right">
+    //           <h2>Career</h2>
+    //           <h3 id="primaryIndustry">Primary Industry Interest: {initPrimaryIndustry()}</h3>
+    //           <h3 id="secondaryIndustry">Secondary Industry Interest: {initSecondaryIndustry()}</h3>
+    //           <h3 id="cities">Particular Cities of Interest: {initCities()}</h3>
+    //         </div>
+    //       </div>
+    //
+    //       <button type="button" id="editButton" onClick={refresh}>edit</button>
+    //       <button id="logout">Logout</button>
+    //     </div>
+    // </div>
+   );
 };
